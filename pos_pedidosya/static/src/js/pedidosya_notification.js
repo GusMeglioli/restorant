73953d044ed3cfaca12f117e97e62991a08b80d0
Gustavo Meglioli <gustavomeglioli@gmail.com>
2026-02-25 02:16:26 -0300

    deploy: pedidosya_notification.js

--- pos_pedidosya/static/src/js/pedidosya_notification.js
+++ pos_pedidosya/static/src/js/pedidosya_notification.js
@@ -1,9 +1,3 @@
 /** @odoo-module **/
-
-/**
- * PedidosYa POS Frontend - punto de entrada.
- * La lógica del botón está en pedidosya_floor_patch.js (HTML/JS puro).
- * Este archivo existe para que el manifest lo cargue en el bundle del POS.
- */
-
 import "./pedidosya_floor_patch";
+import "./pedidosya_vendor_btn";
